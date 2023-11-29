25a22b81c01c687ca4706474bbfa53eca442f7ef
elsamdev <escaparateura@gmail.com>
2023-11-28 20:14:13 -0400

    main

--- pruebasjs/codigosbase/script.js
+++ pruebasjs/codigosbase/script.js
@@ -1,10 +1,12 @@
-const slider = document.querySelector(".slider__container")
-const puntos = document.querySelectorAll(".punto")
-const sliderNum = document.querySelectorAll(".slider__container-card")
+const slider = document.querySelector(".slider__container");
+const puntos = document.querySelectorAll(".punto");
+const skills = document.querySelectorAll(".slider__skills-item");
+const sliderNum = document.querySelectorAll(".slider__container-card");
+const modal = document.querySelector(".modal");
+const cerrarModal = document.querySelector(".modal__cerrar");
 const porcentajeSlider = 100 / sliderNum.length;
 puntos.forEach( (cadaPunto, i)=> {
     puntos[i].addEventListener(`click`,()=>{
-
         let posicion = i;
         console.log("i "+posicion);
         let operacion = posicion * -(porcentajeSlider);
@@ -18,3 +20,54 @@ puntos.forEach( (cadaPunto, i)=> {
     })
 
 })
+
+skills.forEach( (cadaSkill, i)=> {
+    skills[i].addEventListener(`click`,()=>{
+        let posicion = i;
+        console.log("Skills "+posicion);
+        modal.style.display = "block";
+        
+        // puntos.forEach ((cadaPunto , i)=>{
+        //     puntos[i].classList.remove("activo")
+        // })
+        // puntos[posicion].classList.add("activo");
+    })
+
+});
+
+
+cerrarModal.addEventListener('click', function(){
+    modal.style.display = "none";
+});
+
+///////// Acordeon  //////////////
+
+const abrirCerrarAcordeo = document.querySelectorAll(".acordeon__header");
+const cuerpoAcordeon = document.querySelectorAll(".acordeon__body");
+const flechaAcordeon = document.querySelectorAll(".flecha__acordeon");
+function cargarMaxW(){
+    abrirCerrarAcordeo.forEach( (cadaTitulo, i)=> {
+        cuerpoAcordeon[i].style.maxHeight = "0px";
+    });
+}
+
+window.onload = cargarMaxW();
+abrirCerrarAcordeo.forEach( (cadaTitulo, i)=> {
+    abrirCerrarAcordeo[i].addEventListener(`click`,()=>{
+        let posicion = i;
+        if ((cuerpoAcordeon[posicion].style.maxHeight === "0px") ) {
+            abrirCerrarAcordeo.forEach ((cadaBodyAcordeon , i)=>{
+            flechaAcordeon[i].classList.remove("acordeon__flecha");
+            cuerpoAcordeon[i].style.maxHeight = "0px";
+         })
+            flechaAcordeon[i].classList.add("acordeon__flecha");
+            cuerpoAcordeon[i].style.maxHeight = cuerpoAcordeon[i].scrollHeight + "px";
+            console.log("esta cerrado")
+        }else{
+         flechaAcordeon[i].classList.remove("acordeon__flecha");
+         cuerpoAcordeon[i].style.maxHeight = "0px";
+         console.log("esta abierto")
+        }
+    })
+
+});
