541ebeff6d63a715181180edc681ab059467e65e
elsamdev <escaparateura@gmail.com>
2023-12-05 11:38:59 -0400

    main

--- Digorno/js/script.js
+++ Digorno/js/script.js
@@ -9,21 +9,22 @@ const reviewsButtons = document.querySelectorAll(".reviews__buttons")
 
 reviewsButtons.forEach((buttonSlider, i)=>{
     
-    reviewsButtons[i].addEventListener("click", function(){
+    reviewsButtons[i].addEventListener("click", function(event){
+        event.preventDefault();
         reviewsButtons.forEach(button =>{
             button.style.width = "50px";
         })
         reviewsButtons[i].style.width = "200px";
         sliderContainer[i].style.transform = "translatex(0%)";
         if(i === 0){
-            sliderContainer[i+1].style.transform = "translatex(200%)";
-            sliderContainer[i+2].style.transform = "translatex(200%)";
+            sliderContainer[i+1].style.transform = "translatex(400%)";
+            sliderContainer[i+2].style.transform = "translatex(400%)";
         }else if (i === 1){
-            sliderContainer[i+1].style.transform = "translatex(200%)";
-            sliderContainer[i-1].style.transform = "translatex(200%)";
+            sliderContainer[i+1].style.transform = "translatex(400%)";
+            sliderContainer[i-1].style.transform = "translatex(400%)";
         }else{
-            sliderContainer[i-1].style.transform = "translatex(200%)";
-            sliderContainer[i-2].style.transform = "translatex(200%)";
+            sliderContainer[i-1].style.transform = "translatex(400%)";
+            sliderContainer[i-2].style.transform = "translatex(400%)";
         }
         
         
