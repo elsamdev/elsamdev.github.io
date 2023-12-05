f83f26cbef4533607e5011d6b4307168116d6e15
elsamdev <escaparateura@gmail.com>
2023-12-05 11:24:14 -0400

    sam

---
+++ Digorno/js/script.js
@@ -0,0 +1,32 @@
+const sliderContainer = document.querySelectorAll(".reviews__transition")
+const reviewsButtons = document.querySelectorAll(".reviews__buttons")
+
+// sliderContainer.forEach((slider, i) =>{
+// console.log (slider)
+
+
+// })
+
+reviewsButtons.forEach((buttonSlider, i)=>{
+    
+    reviewsButtons[i].addEventListener("click", function(){
+        reviewsButtons.forEach(button =>{
+            button.style.width = "50px";
+        })
+        reviewsButtons[i].style.width = "200px";
+        sliderContainer[i].style.transform = "translatex(0%)";
+        if(i === 0){
+            sliderContainer[i+1].style.transform = "translatex(200%)";
+            sliderContainer[i+2].style.transform = "translatex(200%)";
+        }else if (i === 1){
+            sliderContainer[i+1].style.transform = "translatex(200%)";
+            sliderContainer[i-1].style.transform = "translatex(200%)";
+        }else{
+            sliderContainer[i-1].style.transform = "translatex(200%)";
+            sliderContainer[i-2].style.transform = "translatex(200%)";
+        }
+        
+        
+        
+    })
+})
--- pruebasjs/codigosbase/pokeapi/script.js
+++ pruebasjs/codigosbase/pokeapi/script.js
@@ -11,6 +11,10 @@ const ColorType = [
 ["fire","#ea3333"],
 ["normal","#915151"]
 ];
+const pokeBuscador = document.getElementById("buscador");
+const pokeBuscadorResultadoContainer = document.querySelector(".resultado_busqueda");
+// const pokeBuscadorFoto = document.querySelector(".pokemon__tipos");
+// const pokeBuscadorNombre = document.querySelector(".pokemon__tipos");
 document.addEventListener('DOMContentLoaded', function() {
 fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
   .then(response => response.json())
@@ -32,37 +36,38 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
     pokemonList.forEach((pokemon, i) =>{
     pokemonList[i].onclick = function(){
         const pokeName = pokemonList[i].textContent;
-        const pokeNameLi = pokeName.split(" ");
+        
         // pokemonList[i].style.color = "red";
-        console.log(pokeNameLi)
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNameLi[1]}`)
-        .then(response => response.json())
-        .then(data => {
-        // Manipula los datos obtenidos
-            const sprites = data.sprites;
-            const urlImg = sprites.front_default;
-            pokePhoto.src = urlImg;
-            //obtener los tipos
-            const typesPoke = data.types.map(type => type.type.name);
+        console.log(pokeName)
+        buscarImgApi(pokeName,"pokedex")
+        // fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNameLi[1]}`)
+        // .then(response => response.json())
+        // .then(data => {
+        // // Manipula los datos obtenidos
+        //     const sprites = data.sprites;
+        //     const urlImg = sprites.front_default;
+        //     pokePhoto.src = urlImg;
+        //     //obtener los tipos
+        //     const typesPoke = data.types.map(type => type.type.name);
            
-            pokeTypes.innerHTML = "";
-            typesPoke.forEach((types, i) =>{
-                var cadaType = document.createElement("div")
-                ////////////aquiiiiiiiiiiiiii
-                var matrizColor = ColorType.filter(fila => fila[0] === typesPoke[i]);
-                var colorDelTipo = matrizColor.map(fila => fila[1]);
-                cadaType.style.backgroundColor = colorDelTipo;
-                console.log(colorDelTipo);
-                cadaType.className = `Pokemon__types`;
-                cadaType.textContent = typesPoke[i];
+        //     pokeTypes.innerHTML = "";
+        //     typesPoke.forEach((types, i) =>{
+        //         var cadaType = document.createElement("div")
+        //         ////////////aquiiiiiiiiiiiiii
+        //         var matrizColor = ColorType.filter(fila => fila[0] === typesPoke[i]);
+        //         var colorDelTipo = matrizColor.map(fila => fila[1]);
+        //         cadaType.style.backgroundColor = colorDelTipo;
+        //         console.log(colorDelTipo);
+        //         cadaType.className = `Pokemon__types`;
+        //         cadaType.textContent = typesPoke[i];
                 
-                pokeTypes.appendChild(cadaType);
-            });
-        })
-        .catch(error => {
-        // Maneja cualquier error que ocurra durante la solicitud
-        console.log(error);
-        });
+        //         pokeTypes.appendChild(cadaType);
+        //     });
+        // })
+        // .catch(error => {
+        // // Maneja cualquier error que ocurra durante la solicitud
+        // console.log(error);
+        // });
 
     }
 
@@ -75,13 +80,109 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
     // Maneja cualquier error que ocurra durante la solicitud
     console.log(error);
   });
+
+  return pokemonList;
 });
 
-    function pokemonTypeColor(color){
+function buscarImgApi(name, lugar){
+  const pokeNameLi = name.split(" ");
+ 
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNameLi[1]}`)
+  .then(response => response.json())
+  .then(data => {
+  // Manipula los datos obtenidos
+      const sprites = data.sprites;
+      const urlImg = sprites.front_default;
+      if(lugar === "buscador"){
+        var buscadorContainer = document.createElement("div")
+        buscadorContainer.className = "minipoke__container"
+    
+        var buscadorContainerImg = document.createElement("img")
+        buscadorContainerImg.className = "minipoke__img";
+        buscadorContainerImg.src = urlImg;
+        var buscadorContainerName = document.createElement("div")
+        buscadorContainerName.className = "minipoke__name";
+        buscadorContainerName.textContent = name;
+        // console.log(elemento.textContent)
+        buscadorContainer.appendChild(buscadorContainerImg)
+        buscadorContainer.appendChild(buscadorContainerName)
+        pokeBuscadorResultadoContainer.appendChild(buscadorContainer);
+    }else{
+      pokePhoto.src = urlImg;
+      //obtener los tipos
+      const typesPoke = data.types.map(type => type.type.name);
+     
+      pokeTypes.innerHTML = "";
+      typesPoke.forEach((types, i) =>{
+          var cadaType = document.createElement("div")
+          ////////////aquiiiiiiiiiiiiii
+          var matrizColor = ColorType.filter(fila => fila[0] === typesPoke[i]);
+          var colorDelTipo = matrizColor.map(fila => fila[1]);
+          cadaType.style.backgroundColor = colorDelTipo;
+          console.log(colorDelTipo);
+          cadaType.className = `Pokemon__types`;
+          cadaType.textContent = typesPoke[i];
+          
+          pokeTypes.appendChild(cadaType);
+      
+      });
 
     }
+  })
+  .catch(error => {
+  // Maneja cualquier error que ocurra durante la solicitud
+  console.log(error);
+  });
 
 
+}
+
+function Buscador(letras) {
+  const elementos = [...document.querySelectorAll(".cada__poke")];
+  pokeBuscadorResultadoContainer.innerHTML = "";
+  if (letras.trim() === "") {
+    elementos.forEach(elemento => {
+      pokeBuscadorResultadoContainer.innerHTML = "";
+    });
+    return; // Salir de la función si no hay letras escritas
+  }
+  elementos.forEach(elemento => {
+    if (elemento.textContent.toLowerCase().includes(letras)) {
+      elemento.style.display = "block";
+     
+      buscarImgApi(elemento.textContent, "buscador")
+      //buscador desplegable
+      // var buscadorContainer = document.createElement("div")
+      // buscadorContainer.className = "minipoke__container"
+
+      // var buscadorContainerImg = document.createElement("div")
+      // buscadorContainerImg.className = "minipoke__img";
+
+      // var buscadorContainerName = document.createElement("div")
+      // buscadorContainerName.className = "minipoke__name";
+      // buscadorContainerName.textContent = elemento.textContent;
+      // console.log(elemento.textContent)
+      // buscadorContainer.appendChild(buscadorContainerImg)
+      // buscadorContainer.appendChild(buscadorContainerName)
+      // pokeBuscadorResultadoContainer.appendChild(buscadorContainer);
+    } else {
+      // elemento.style.display = "none";
+    }
+  });
+
+  // console.log(elementos);
+}
+
+pokeBuscador.addEventListener("input", function() {
+  const escribiendo = pokeBuscador.value.toLowerCase();
+  Buscador(escribiendo);
+});
 
 
-    // Hacer una solicitud GET a la API de PokeAPI para obtener información sobre un Pokémon específico
+    const pata = [
+      'Bulbasaur',
+      'Charmander',
+      'Squirtle',
+      'Pikachu',
+      // Agrega más Pokémon aquí
+    ];
