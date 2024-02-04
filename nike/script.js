8825ac5cceb9899ce126f89132e1b26ef484ef03
elsamdev <149830803+elsamdev@users.noreply.github.com>
2024-02-04 14:39:21 -0400

    Add files via upload

--- nike/script.js
+++ nike/script.js
@@ -6,14 +6,15 @@ const numProductsElement = document.getElementById("num-products");
 
 // Obtener el select de filtrar por categoría
 const categorySelect = document.getElementById("category");
-
+closeLightbox()
 // Obtener el campo de texto para buscar por título
 const searchInput = document.getElementById("search-input");
-
+let productsData;
 // Obtener los datos de los productos desde el archivo JSON
 fetch("products.json")
   .then((response) => response.json())
   .then((data) => {
+    productsData = data;
     // Filtrar por categoría y título
     const filterProducts = (category, title) => {
       productsContainer.innerHTML = ""; // Limpiar el contenedor de productos
@@ -23,9 +24,13 @@ fetch("products.json")
         data.forEach((product) => {
           createProductCard(product);
         });
+
       } else {
         // Filtrar por categoría
         let filteredProducts = data;
+
+
+        
         if (category !== "") {
           filteredProducts = filteredProducts.filter((product) =>
             product.category.includes(category)
@@ -53,16 +58,49 @@ fetch("products.json")
     const createProductCard = (product) => {
       const card = document.createElement("div");
       card.classList.add("product-card");
+      card.dataset.id = product.id; // Establecer el valor del atributo "data-id" con el valor del id del producto
 
       const image = document.createElement("img");
       image.src = product.images[0];
       image.alt = product.title;
       card.appendChild(image);
+    
+      const thumbnailContainer = document.createElement("div");
+      thumbnailContainer.classList.add("thumbnail-container"); // Agrega una clase CSS si deseas estilarlo
+      card.appendChild(thumbnailContainer);
+      let additionalImages = 0;
+      let isButtonAdded = false;
 
+      product.thumbnail.forEach((thumbnailUrl, index) => {
+        
+          
+        if (index !== 0 && thumbnailUrl.length <= 4) { // Ignorar la primera imagen del thumbnail
+          const thumbnailImage = document.createElement("img");
+          thumbnailImage.src = thumbnailUrl;
+          thumbnailContainer.appendChild(thumbnailImage);
+        }
+        else if (thumbnailUrl.length >= 5) {
+          if (index !== 0 && index !== 0 && index <= 3) { // Ignorar la primera imagen del thumbnail
+            const thumbnailImage = document.createElement("img");
+            thumbnailImage.src = thumbnailUrl;
+            thumbnailContainer.appendChild(thumbnailImage);
+          }
+          else if (index !== 0 && !isButtonAdded) {
+            const thumbnailButton = document.createElement("button");
+            const thumbnailRestantes = product.thumbnail.length - 4;
+            thumbnailButton.textContent = thumbnailRestantes + "+";
+            thumbnailButton.classList.add("thumbnail-button"); // Agrega la clase CSS deseada
+            thumbnailContainer.appendChild(thumbnailButton);
+            isButtonAdded = true; // Cambia el estado de la variable para indicar que el botón ya ha sido agregado
+          }
+        }
+      });
+
+    
       const title = document.createElement("h2");
       title.textContent = product.title;
       card.appendChild(title);
-
+    
       const button = document.createElement("button");
       button.classList.add("whatsapp-button");
       button.addEventListener("click", () => {
@@ -72,7 +110,7 @@ fetch("products.json")
       });
       button.innerHTML = '<i class="fab fa-whatsapp"></i> Consultar por modelo';
       card.appendChild(button);
-
+    
       productsContainer.appendChild(card);
     };
 
@@ -89,7 +127,15 @@ fetch("products.json")
 // Seleccionar la list
     // Seleccionar la lista
 const listaCategory = document.getElementById("category2");
+const logo = document.querySelector("#logo");
+const inputElement = document.querySelector('#search-input');
 let categoria = ""
+
+logo.addEventListener('click', () => {
+  inputElement.value = '';
+  filterProducts("", "");
+});
+
 listaCategory.addEventListener("click", (event) => {
   // Obtener el elemento "a" que se ha hecho clic
   const aSeleccionado = event.target;
@@ -133,38 +179,167 @@ listaCategory.addEventListener("click", (event) => {
   function setupLightbox() {
     const productCards = document.querySelectorAll('.product-card');
     const lightbox = document.getElementById('lightbox');
+    // const carouselContainer =  document.getElementById('carouselContainer');
     const lightboxImage = document.getElementById('lightbox-image');
     const lightboxTitle = document.getElementById('lightbox-title');
     const lightboxButton = document.getElementById('lightbox-button');
     const closeButton = document.getElementById('close-button');
     
-  
+    const carouselContainer = document.getElementById('carousel-container');
+    const carousel = document.getElementById('carousel');
+    const carouselPrev = document.getElementById('carousel-prev');
+    const carouselNext = document.getElementById('carousel-next');
+    const thumbnails = document.getElementById('thumbnails');
+   
     productCards.forEach((card) => {
       const image = card.querySelector('.product-card img'); // Corregir la clase del selector
       const title = card.querySelector('h2');
       const button = card.querySelector('.whatsapp-button');
-  
+      const imagethumbnail = card.querySelectorAll('.thumbnail-container img');
+      const thumbnailButton = card.querySelector('.thumbnail-button');
+
+      let currentIndex = 0;
+
+
+        //abrir al apretar imagen principal
       image.addEventListener('click', () => {
-        const imageUrl = image.src;
-        const imageTitle = title.textContent;
-        lightboxImage.src = imageUrl;
-        lightboxTitle.textContent = imageTitle;
-        lightboxButton.innerHTML = button.innerHTML;
-        
-        lightboxButton.addEventListener("click", () => {
-          const message = encodeURIComponent(`Quiero más información del modelo: ${title.textContent}`);
-          const url = `https://wa.me/13213287507?text=${message}`;
-          window.open(url);
-        });
-        lightbox.style.display = 'flex';
+          currentIndex = 0
+          openLightbox()
       });
+      
+      //abrir al apretar imagen thumbnail
+      imagethumbnail.forEach((image, index) => {
+        image.addEventListener('click', () => {
+          currentIndex = index+1
+              openLightbox()
+          })
+      });
+      
+      //abrir al apretar imagen boton +
+      if (thumbnailButton) {
+        thumbnailButton.addEventListener('click', () => {
+          currentIndex = 3;
+          openLightbox();
+        });
+      }
+
+      function openLightbox(){
+        const imageTitle = title.textContent;
+        const productId = card.dataset.id;
+
+        const images = productsData.filter(product => product.id === productId)[0].images;
+
+  
+          // Recorrer las imágenes del producto y crear elementos <img> en el carrusel y miniaturas
+          images.forEach((image, index) => {
+          const img = document.createElement('img');
+          img.src = image;
+          carousel.appendChild(img);
+
+          const thumbnail = document.createElement('img');
+          thumbnail.src = image;
+          thumbnail.classList.add('thumbnail');
+          thumbnail.addEventListener('click', () => {
+            currentIndex = index;
+            showCurrentImage();
+          });
+          thumbnails.appendChild(thumbnail);
+        });
+
+
+        
+      
+
+        // Función para mostrar la imagen actual en el carrusel
+        const showCurrentImage = () => {
+          // Obtener todas las imágenes en el carrusel y miniaturas
+          const images = carousel.getElementsByTagName('img');
+          const thumbs = thumbnails.getElementsByTagName('img');
+          console.log(images.length)
+          // Ocultar todas las imágenes y miniaturas
+          for (let i = 0; i < images.length; i++) {
+            images[i].style.display = 'none';
+            thumbs[i].classList.remove('active');
+          }
+
+          // Mostrar la imagen actual y miniatura activa
+          images[currentIndex].style.display = 'block';
+          thumbs[currentIndex].classList.add('active');
+        };
+
+        // Mostrar la imagen actual
+        showCurrentImage();
+
+          const navigateNext = () => {
+          currentIndex++;
+          if (currentIndex >= images.length) {
+            currentIndex = 0;
+          }
+          showCurrentImage();
+        };
+
+        // Función para navegar hacia atrás
+        const navigatePrev = () => {
+          currentIndex--;
+          if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+          }
+          showCurrentImage();
+        };
+
+        // Agregar eventos de clic a las flechas de navegación
+        carouselNext.addEventListener('click', navigateNext);
+        carouselPrev.addEventListener('click', navigatePrev);
+
+    //***************************** */
+
+          
+            // lightboxImage.src = imageUrl;
+            lightboxTitle.textContent = imageTitle;
+            lightboxButton.innerHTML = button.innerHTML;
+          
+            lightboxButton.addEventListener("click", () => {
+              const message = encodeURIComponent(`Quiero más información del modelo: ${title.textContent}`);
+              const url = `https://wa.me/13213287507?text=${message}`;
+              window.open(url);
+            });
+            lightbox.style.display = 'flex';
+      }
+
+
+
+
+
+
+
+      
     });
   
     closeButton.addEventListener('click', () => {
       lightbox.style.display = 'none';
+      closeLightbox()
     });
   }
   
   // Llamar a la función inicialmente
  // Seleccionar la lista
 
+ function closeLightbox() {
+  // Restablecer los valores a blanco
+  const lightboxImage = document.getElementById('lightbox-image');
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
+
+  const lightboxTitle = document.getElementById('lightbox-title');
+  lightboxTitle.textContent = '';
+  const carouselContainer =  document.getElementById('carouselContainer');
+  carouselContainer.innerHTML = '';
+
+  const thumbnails =  document.getElementById('thumbnails');
+  thumbnails.innerHTML = '';
+
+  const carousel =  document.getElementById('carousel');
+  carousel.innerHTML = '';
+  // Cerrar el lightbox
+  lightbox.style.display = 'none';
+}
