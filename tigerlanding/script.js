a0140b4a9e6c1af6148ce66fd93298eda3b147bd
elsamdev <149830803+elsamdev@users.noreply.github.com>
2024-04-24 13:10:41 -0400

    main

--- tigerlanding/script.js
+++ tigerlanding/script.js
@@ -1,7 +1,7 @@
 let coins = document.querySelectorAll('.pcoin_rain_img');
 
 coins.forEach((coin, index) => {
-  let randomDelay = Math.random() * 0 - 2.9; // Genera un número aleatorio entre -1 y 1
+  let randomDelay = Math.random() * 0.1 - 2.9; // Genera un número aleatorio entre -1 y 1
   let randomDuration = Math.random() * 1 + 2; // Genera un número aleatorio entre 1.2 y 1.7
   
   coin.style.animationDelay = `${randomDelay}s`;
